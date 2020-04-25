8094180fa3c4b67b5a1223f9bfac1cf823bece16
Steve Boyer <steveboyer@Macbook-2.local>
2020-04-25 15:04:48 -0500

    movies create route hitting

--- resources/movies.py
+++ resources/movies.py
@@ -4,7 +4,12 @@ from flask import Blueprint
 
 movies = Blueprint('movies', 'movies')
 
-
+# GET /api/v1/movies
 @movies.route('/', methods=['GET'])
 def movies_index():
   return "movies resource working"
+
+# CREATE /api/v1/movies/
+@movies.route('/', methods=['POST'])
+def create_movie():
+  return "movies create route hitting"
