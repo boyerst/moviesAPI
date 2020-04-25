11c75f1fd93e32f92a2c83a720116c2f89e68628
Steve Boyer <steveboyer@Macbook-2.local>
2020-04-25 15:11:22 -0500

    it gets data from request body w/ request.get_json

--- resources/movies.py
+++ resources/movies.py
@@ -1,15 +1,20 @@
 import models
 
-from flask import Blueprint
+from flask import Blueprint, request
 
 movies = Blueprint('movies', 'movies')
+
 
 # GET /api/v1/movies
 @movies.route('/', methods=['GET'])
 def movies_index():
   return "movies resource working"
 
+
+
 # CREATE /api/v1/movies/
 @movies.route('/', methods=['POST'])
 def create_movie():
+  payload = request.get_json()
+  print(payload)
   return "movies create route hitting"
