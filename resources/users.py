d3c3592e5e1b0c206f4298f178f96ba6fe9ef2d8
Steve Boyer <steveboyer@Macbook-2.local>
2020-04-28 19:18:53 -0500

    Build on users.py: added register route, register route hitting

--- resources/users.py
+++ resources/users.py
@@ -1,6 +1,6 @@
 import models 
 
-from flask import Blueprint
+from flask import Blueprint, request
 
 
 users = Blueprint('users', 'users')
@@ -8,3 +8,9 @@ users = Blueprint('users', 'users')
 @users.route('/', methods=['GET'])
 def test_user_resource(): 
   return "user resource hitting" 
+
+
+@users.route('/register', methods=['POST'])
+def register():
+  print(request.get_json())
+  return "register route hitting, check terminal" 
