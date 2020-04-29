462139874c86907b8e932ea3937f3f386c879ac6
Steve Boyer <steveboyer@Macbook-2.local>
2020-04-28 19:37:51 -0500

    Build on users.py: user regristration functioning

--- resources/users.py
+++ resources/users.py
@@ -1,6 +1,8 @@
 import models 
 
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
+
+from flask_bcrypt import generate_password_hash
 
 
 users = Blueprint('users', 'users')
@@ -12,5 +14,30 @@ def test_user_resource():
 
 @users.route('/register', methods=['POST'])
 def register():
-  print(request.get_json())
-  return "register route hitting, check terminal" 
+  payload = request.get_json()
+  payload['email'] = payload['email'].lower()
+  payload['username'] = payload['username'].lower()
+  try:
+    models.User.get(models.User.email == payload['email'])
+    return jsonify(
+      data={},
+      message=f"user with the email {payload['email']} already exists",
+      status=401
+    ), 401
+  except models.DoesNotExist:
+    created_user = models.User.create(
+      username=payload['username'],
+      email=payload['email'],
+      password=generate_password_hash(payload['password'])
+    )
+    print(created_user)
+    login_user(created_user)
+    created_user_dict = model_to_dict(created_user)
+    print(created_user_dict)
+    print(type(created_user_dict['password']))
+    created_user_dict.pop('password')
+    return jsonify(
+      data=created_user_dict,
+      message=f"Successfully registered user {created_user_dict['email']}",
+      status=201
+    ), 201
