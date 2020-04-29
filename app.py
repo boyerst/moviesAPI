efdc37718f06f54ee4664130d235f227c9d5c876
Steve Boyer <steveboyer@Macbook-2.local>
2020-04-28 19:16:58 -0500

    Build on resources/: added users.py + route, users route hitting

--- app.py
+++ app.py
@@ -2,9 +2,13 @@ from flask import Flask, jsonify
 
 from resources.movies import movies
 
+from resources.users import users
+
 import models
 
 from flask_cors import CORS
+
+from flask_login import LoginManager
 
 
 DEBUG=True
@@ -13,9 +17,17 @@ PORT=8000
 
 app = Flask(__name__)
 
+app.secret_key = "secret time"
+
+login_manager = LoginManager()
+
+login_manager.init_app(app) #passing in app object, defined above
+
 CORS(movies, origins=['http://localhost:3000'], supports_credentials=True)
+CORS(users, origins=['http://localhost:3000'], supports_credentials=True)
 
 app.register_blueprint(movies, url_prefix='/api/v1/movies')
+app.register_blueprint(users, url_prefix='/api/v1/users')
 
 
 
---
+++ resources/users.py
@@ -0,0 +1,10 @@
+import models 
+
+from flask import Blueprint
+
+
+users = Blueprint('users', 'users')
+
+@users.route('/', methods=['GET'])
+def test_user_resource(): 
+  return "user resource hitting" 
